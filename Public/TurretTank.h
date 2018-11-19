2cd6105ae8e773831e55e88d01f0f6690fb46854
isnizal <31788317+isnizal@users.noreply.github.com>
2018-11-19 17:02:49 +0800

    creating turret tank

---
+++ Public/TurretTank.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TurretTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAVALRYFIREV01_API UTurretTank : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
